f0234a6f57a034b32487390cc16f3f3c76c9c8eb
xl000 <l_xb@foxmail.com>
2020-08-03 23:13:02 +0800

    unsafe

---
+++ tests/test_unsafe.rs
@@ -0,0 +1,54 @@
+///
+/// # Unsafe Operations
+///
+/// Unsafe annotations in Rust are used to bypass protections put in place by the compiler;
+/// specifically, there are four primary things that unsafe is used for:
+///
+/// * dereferencing raw pointers
+/// * calling functions or methods which are `unsafe`(including calling a function over FFI)
+/// * accessing or modifying static mutable variables
+/// * implementing unsafe traits
+///
+/// ## Raw Pointers
+///
+/// Raw pointers `*` and references `&T` function similarly, but references are always safe
+/// because they are guaranteed to point to valid data due to the borrow checker. Dereferencing
+/// a raw pointer can only be done through an unsafe block.
+///
+/// ## Calling Unsafe Functions
+///
+/// Some functions can be declared as `unsafe`, meaning it is the programmer's responsibility to
+/// ensure correctness instead of the compiler's.
+///
+/// ## Foreign Function Interface
+///
+/// Rust provides a Foreign Function Interface(FFI) to C libraries. Foreign functions must be
+/// declared inside an `extern` block annotated with a `#[link]` attribute containing the name
+/// of the foreign library.
+///
+// // this extern block links to the libc library
+// #[link(name = "c")]
+// extern "C" {
+//     fn abs(input: i32) -> i32;
+//     fn write(fd: i32, data: *const u8, len: usize) -> isize;
+// }
+use std::slice;
+#[test]
+fn test_unsafe() {
+    let raw_p: *const u32 = &10;
+    unsafe {
+        assert_eq!(10, *raw_p);
+    }
+
+    let v = vec![1, 2, 3, 4];
+    let pointer = v.as_ptr();
+    let length = v.len();
+    // let data = b"hello world\n";
+    unsafe {
+        let my_slice: &[u32] = slice::from_raw_parts(pointer, length);
+        assert_eq!(v.as_slice(), my_slice);
+
+        //     assert_eq!(abs(-5), 5);
+        //     write(1, data.as_ptr(), data.len());
+    }
+}
