0bafd84a052eba7f559aaa4aea42ab5647cc981b
Li Xiaobin <l_xb@foxmail.com>
2021-07-20 01:08:10 +0800

    kv server

--- training/kv/src/pb.rs
+++ training/kv/src/pb.rs
@@ -1,3 +1,64 @@
+use bytes::{Bytes, BytesMut};
+use prost::Message;
+use std::convert::TryFrom;
+
 mod abi;
 
 pub use abi::*;
+
+impl Response {
+    pub fn new(key: String, value: Vec<u8>) -> Self {
+        Self {
+            code: 0,
+            key,
+            value,
+        }
+    }
+
+    pub fn not_found(key: String) -> Self {
+        Self {
+            code: 404,
+            key,
+            ..Default::default()
+        }
+    }
+
+    pub fn not_impl() -> Self {
+        Self {
+            code: 500,
+            ..Default::default()
+        }
+    }
+}
+
+impl TryFrom<BytesMut> for Request {
+    type Error = prost::DecodeError;
+
+    fn try_from(buf: BytesMut) -> Result<Self, Self::Error> {
+        Message::decode(buf)
+    }
+}
+
+impl TryFrom<BytesMut> for Response {
+    type Error = prost::DecodeError;
+
+    fn try_from(buf: BytesMut) -> Result<Self, Self::Error> {
+        Message::decode(buf)
+    }
+}
+
+impl From<Response> for Bytes {
+    fn from(msg: Response) -> Self {
+        let mut buf = BytesMut::new();
+        msg.encode(&mut buf).unwrap();
+        buf.freeze()
+    }
+}
+
+impl From<Request> for Bytes {
+    fn from(msg: Request) -> Self {
+        let mut buf = BytesMut::new();
+        msg.encode(&mut buf).unwrap();
+        buf.freeze()
+    }
+}
--- training/kv/src/server.rs
+++ training/kv/src/server.rs
@@ -1,12 +1,79 @@
 mod pb;
-use pb::*;
-use prost::Message;
+use std::sync::Arc;
 
-fn main() {
-    let get = RequestGet {
-        key: "hello".to_owned(),
-    };
-    let mut buf = Vec::new();
-    get.encode(&mut buf).unwrap();
-    println!("{:?}", buf);
+use dashmap::DashMap;
+use pb::{request::*, *};
+
+use anyhow::Result;
+use futures::{SinkExt, StreamExt};
+use std::convert::TryInto;
+use tokio::net::TcpListener;
+use tokio_util::codec::length_delimited::LengthDelimitedCodec;
+use tracing::info;
+
+#[derive(Debug)]
+struct ServerState {
+    store: DashMap<String, Vec<u8>>,
+}
+
+impl Default for ServerState {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ServerState {
+    pub(crate) fn new() -> Self {
+        Self {
+            store: DashMap::new(),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let state = Arc::new(ServerState::new());
+    let addr = "0.0.0.0:8888";
+    let listener = TcpListener::bind(addr).await?;
+
+    info!("Listening on {}", addr);
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        info!("New client {:?} accepted", addr);
+
+        let shared = state.clone();
+
+        tokio::spawn(async move {
+            let mut stream = LengthDelimitedCodec::builder()
+                .length_field_length(2)
+                .new_framed(stream);
+
+            while let Some(Ok(buf)) = stream.next().await {
+                let msg: Request = buf.try_into()?;
+                info!("Got a command: {:?}", msg);
+                let response = match msg.command {
+                    Some(Command::Get(RequestGet { key })) => match shared.store.get(&key) {
+                        Some(v) => Response::new(key, v.value().to_vec()),
+                        None => Response::not_found(key),
+                    },
+                    Some(Command::Put(RequestPut { key, value })) => {
+                        shared.store.insert(key.clone(), value.clone());
+                        Response::new(key, value)
+                    }
+                    Some(Command::Del(RequestDel { key })) => match shared.store.remove(&key) {
+                        Some((k, v)) => Response::new(k, v),
+                        None => Response::not_found(key),
+                    },
+                    None => Response::not_impl(),
+                };
+                stream.send(response.into()).await?;
+            }
+            Ok::<(), anyhow::Error>(())
+        });
+    }
 }
